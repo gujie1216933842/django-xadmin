092805cb0b8032003c197dd12b6f38c41a940eb7
gujie1216933842 <gujientsy@163.com>
2018-11-06 21:42:23 +0800

    django-xadmin

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+"""
+pep8 规范
+第一块区域: python解释器自带包
+第二块区域: 下载的第三方库的包
+第三块区域: 引入自己的的模块
+中间有一个空行隔开
+
+"""
 
 
 # Create your models here.
@@ -21,3 +31,29 @@ class UserProfile(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+
+class EmailVerifyRecode(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u"邮箱验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
+
+    # def __unicode__(self):
+    #     #     return self.code
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=u'轮播图', max_length=100)
+    url = models.CharField(max_length=200, verbose_name=u'访问地址')
+    index = models.IntegerField(default=100, verbose_name=u'顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
