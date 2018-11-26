c61a212c8a4264e734a0a58e54d72012674c776e
gujie1216933842 <gujientsy@163.com>
2018-11-26 23:35:30 +0800

    图片资源文件

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import View
 from .models import CoursesOrg, CityDict
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 
 class OrgView(View):
@@ -12,10 +13,19 @@ class OrgView(View):
     """
 
     def get(self, request):
-        all_orgs = CoursesOrg.objects.all()  # 所有机构
+        all_orgs = CoursesOrg.objects.all()  #
+        org_nums = all_orgs.count()
 
         all_citys = CityDict.objects.all()  # 所有城市
 
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_orgs, 3, request=request)
+        print(p)
+        orgs = p.page(page)
+        print(orgs)
 
-
-        return render(request, 'org-list.html', locals())
+        return render(request, 'org-list.html',locals())
