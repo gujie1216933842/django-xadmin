2b2ff102f6c442631482b111ffbf3d79f4f44cce
gujie <jie.j.gu@rrd.com>
2018-11-29 10:40:55 +0800

    url 配置media路径修改

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import View
 from .models import CoursesOrg, CityDict
-from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
