40a85085f848fd2bfdab4764882ac4e8ff27d6fc
gujie1216933842 <gujientsy@163.com>
2018-12-01 14:40:40 +0800

    机构详情页面

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from .views import OrgView, AddUserAskView,OrgHomeView
+from .views import OrgView, AddUserAskView,OrgHomeView,OrgCourserView
 
 urlpatterns = [
     # 课程机构
     url(r'^list/$', OrgView.as_view(), name='org_list'),
     url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='org_home'),
+    url(r'^course/(?P<org_id>\d+)/$', OrgCourserView.as_view(), name='org_course'),
 ]
