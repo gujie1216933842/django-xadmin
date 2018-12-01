6a3010dd07fd0735a1b374deb9f642133de6ec4b
gujie1216933842 <gujientsy@163.com>
2018-12-01 14:44:25 +0800

    机构详情页面

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -89,6 +89,7 @@ class OrgHomeView(View):
     """
 
     def get(self, request, org_id):
+        current_page = "home"
         course_org = CoursesOrg.objects.get(id=int(org_id))
         all_courses = course_org.courses_set.all()[:3]
         all_teachers = course_org.teacher_set.all()[:1]
@@ -102,6 +103,7 @@ class OrgCourserView(View):
     """
 
     def get(self, request, org_id):
+        current_page = "course"
         course_org = CoursesOrg.objects.get(id=int(org_id))
         all_courses = course_org.courses_set.all()
         return render(request, 'org-detail-course.html', locals())
