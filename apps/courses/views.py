32ba2dd4018313e5d14ef4a15ba74bb20da9e6aa
gujie1216933842 <gujientsy@163.com>
2018-12-02 13:40:11 +0800

    课程列表页

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, JsonResponse
 
 from .models import CoursesOrg
-
+from organization.models import CityDict
 from operation.models import UserFavorite
 from courses.models import Courses
 from django.shortcuts import render
@@ -15,56 +15,30 @@ from django.shortcuts import render
 
 class CourseListView(View):
     def get(self, request):
-        all_courses = Courses.objects.all()
-
-        all_orgs = CoursesOrg.objects.all()
         all_citys = CityDict.objects.all()  # 所有城市
 
         page = request.GET.get('page', 1)
-        # 取出帅选城市
-        city_id = request.GET.get('city', '')
-        # 取机构类别
-        category = request.GET.get('ct', '')
-        params = {}
-        if city_id:
-            params = {
-                'city_id': city_id
-            }
-
-        if category:
-            params = {
-                'category': category
-            }
-        if city_id and category:
-            params = {
-                'city_id': city_id,
-                'category': category
-            }
 
         sort = request.GET.get('sort', '')
         if sort == 'students':
             order_param = '-students'
-        elif sort == 'courses':
-            order_param = '-course_nums'
+        elif sort == 'hot':
+            order_param = '-hot'
         else:
             order_param = '-id'
 
-        all_orgs = CoursesOrg.objects.filter(**params).order_by(order_param)
-        org_nums = all_orgs.count()  # 筛选条件下的机构总数量
+        all_courses = Courses.objects.all().order_by(order_param)
 
         # 第二个参数表示每页设置2页
-        paginator = Paginator(all_orgs, 2)
+        paginator = Paginator(all_courses, 9)
 
-        current_city_id = int(city_id) if city_id else ''
         current_page = int(page) if page else ''
 
         try:
-            all_orgs = paginator.page(page)
+            all_courses = paginator.page(page)
         except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            all_orgs = paginator.page(1)
+            all_courses = paginator.page(1)
         except EmptyPage:
-            all_orgs = paginator.page(paginator.num_pages)
-
+            all_courses = paginator.page(paginator.num_pages)
 
         return render(request, 'course-list.html', locals())
